227aeff9fe0aac2bfea23d7e7acd2aa6a12ca96b
snowjelly <broadwellconnor@gmail.com>
2023-08-01 11:08:00 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};

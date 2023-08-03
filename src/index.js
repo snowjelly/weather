a13d25a25b1981109948e1579143ae82abbff534
snowjelly <broadwellconnor@gmail.com>
2023-08-03 17:16:47 -0500

    nav open and close animations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import getWeatherData from "./api";
 
 const form = document.querySelector("form");
+const showNavBtn = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   getWeatherData(document.querySelector("#location").value);
+});
+
+showNavBtn.addEventListener("click", (e) => {
+  if (nav.getAttribute("visible") === null) {
+    nav.setAttribute("visible", "");
+  } else {
+    nav.setAttribute("closing", "");
+    nav.addEventListener(
+      "animationend",
+      (e) => {
+        nav.removeAttribute("visible");
+        nav.removeAttribute("closing", "");
+      },
+      { once: true },
+    );
+  }
 });
